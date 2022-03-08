d9a5aea77c2ebdfe75e7556ef263d3f3b817cc71
liam fudge <liamfudge@liams-iMac-2.local>
2022-03-08 17:14:31 -0500

    tempo update v2

--- utils/audioGenerator.py
+++ utils/audioGenerator.py
@@ -34,7 +34,7 @@ def generate_audio(midi, vels, durs, data):
     durs = json.loads(durs)
 
     # 16th note length in ms
-    note_val = ((120 / data['tempo']) * 1000) / 8
+    note_val = int(((120 / data['tempo']) * 1000) / 8)
     overlayPart = AudioSegment.silent(duration=note_val*data['bars'])
 
     for index, i in enumerate(midi):
